fd4027ed173e686d01ce8df7bd9f5287c1fa970c
Yeowol01 <139342008+Yeowol01@users.noreply.github.com>
2023-09-11 19:45:53 +0900

    (2023.09.11) 생성자와 소멸자

---
+++ (2023.09.08) 이름 공간.h
@@ -0,0 +1,122 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+int data = 25;
+
+namespace A
+{
+	void Attack()
+	{
+		cout << "A 개발자 Attack( )" << endl;
+	}
+}
+
+namespace B
+{
+	void Attack()
+	{
+		cout << "B 개발자 Attack( )" << endl;
+	}
+}
+
+void Damage(int hp = 100)
+{
+	hp -= 25;
+	cout << "hp의 값 : " << hp << endl;
+}
+
+// 기본 매개변수는 오른쪽에서 부터 정의해야 합니다.
+void Move(int x, int y, int z = 10)
+{
+	cout << "x의 값 : " << x << endl;
+	cout << "y의 값 : " << y << endl;
+	cout << "z의 값 : " << z << endl;
+}
+
+int max(int a, int b)
+{
+	int n;
+
+	while (b != 0)
+	{
+		n = a % b;
+		a = b;
+		b = n;
+	}
+	return a;
+}
+
+int min(int a, int b)
+{
+	return a * b / max(a, b);
+}
+
+int main()
+{
+#pragma region 범위 지정 연산자
+	// 여러 범위에서 사용되는 식별자를 구분하는데 사용하는 연산자입니다.
+
+	//int data = 100;
+
+	//cout << "data의 값 : " << data << endl;
+	//cout << "data의 값 : " << ::data << endl;
+
+	// 범위 지정 연산자는 전역 변수와 같은 름의 지역 변수가 선언되었을 때 가장 가까운 범위에 선언된 변수의 이름을 사용하는 범위 규칙이 존재하기 때문에 전역 변수가 호출되지 않습니다.
+
+#pragma endregion
+
+#pragma region 이름 공간
+	// 속성을 구분할 수 있도록 유효 범위를 설정하는 영역입니다.
+
+	/*A::Attack();
+	B::Attack();*/
+
+#pragma endregion
+
+#pragma region 기본 매개변수
+	// 함수의 매개변수에 값이 전달되지 않았을 때 기본 값으로 설정되는 매개변수입니다.
+
+	/*int hp = 100;
+
+	Damage();
+	Move(5, 10);*/
+
+	// 기본 매개변수가 있는 위치에 다시 새로운 인수를 넣어줄 수 있습니다.
+	/*Move(5, 10, 25);*/
+
+#pragma endregion
+
+#pragma region 최소 공배수
+
+	int a, b;
+
+	cin >> a >> b;
+
+	cout << "최소공배수 : " << min(a, b);
+
+	// 강사님 방법
+	int x = 0;
+	int y = 0;
+	int count = 1;
+	cin >> x >> y;
+	for (int i = 2; i <= y; i++)
+	{
+		if (x % i == 0 && y % i == 0)
+		{
+			count *= i;
+			x /= i;
+			y /= i;
+
+			i--;
+		}
+	}
+
+	cout << count * x * y << endl;
+
+#pragma endregion
+
+
+	return 0;
+}
+
