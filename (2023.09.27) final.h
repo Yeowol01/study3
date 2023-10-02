cb630bf6551c68f203599120c3349a5d63e2fb36
Yeowol01 <139342008+Yeowol01@users.noreply.github.com>
2023-10-02 19:48:55 +0900

    (2023.10.02) 선형 컨테이너 (Vector)

---
+++ (2023.09.27) final.h
@@ -0,0 +1,36 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+
+#pragma region final
+// 하위 클래스에서 더 이상 재정의할 수 없도록 선언하는 기능입니다.
+
+class Hero
+{
+	virtual void BasicSkill() {};
+	virtual void MagicSkill() {};
+};
+
+class Crusaders : public Hero
+{
+	void BasicSkill() {};
+	void MagicSkill() final {};
+};
+
+class Fighter : public Crusaders
+{
+	void BasicSkill() {};
+	// Crusaders에서 MagicSkill에 final을 선언하였기 때문에 재정의 할 수가 없습니다.
+	// void MagicSkill() {}; ERROR
+};
+
+#pragma endregion
+
+int main()
+{
+
+
+	return 0;
+}
+
--- Solution.cpp
+++ Solution.cpp
@@ -1,34 +1,87 @@
 ﻿#include <iostream>
+#include <vector>
+#include <time.h>
+#include <conio.h>
+#include <Windows.h>
 
 using namespace std;
 
-#pragma region final
-	// 하위 클래스에서 더 이상 재정의할 수 없도록 선언하는 기능입니다.
-
-class Hero
-{
-	virtual void BasicSkill() {};
-	virtual void MagicSkill() {};
-};
-
-class Crusaders : public Hero
-{
-	void BasicSkill() {};
-	void MagicSkill() final {};
-};
-
-class Fighter : public Crusaders
-{
-	void BasicSkill() {};
-	// Crusaders에서 MagicSkill에 final을 선언하였기 때문에 재정의 할 수가 없습니다.
-	// void MagicSkill() {}; ERROR
-};
-
+#pragma region 선형 컨테이너
+	// 데이터를 선형으로 저장하며, 특별한 제약이나 규칙이 없는 일반적인 컨테이너입니다.
 #pragma endregion
+
+int createCount;
 
 int main()
 {
-		 
+#pragma region Vector 컨테이너
+	/*
+	vector<int> vector;
+
+	// push_back() : 뒤 쪽에 데이터를 저장하는 함수
+	//  0    1    2
+	// [10] [20] [30]
+	vector.push_back( 10 );
+	vector.push_back( 20 );
+	vector.push_back( 30 );
+
+	cout << "vector.size : " << vector.size() << endl;
+	cout << "capacity : " << vector.capacity() << endl;
+
+	for ( int i = 0; i < vector.size(); i++ )
+	{
+		cout << vector[i] << endl;
+	}
+
+	vector.push_back( 40 );
+	vector.push_back( 50 );
+
+	//  0    1    2    3
+	// [10] [20] [30] [40]
+	vector.pop_back();
+
+	cout << "vector.size : " << vector.size() << endl;
+	cout << "capacity : " << vector.capacity() << endl;
+
+	for ( int i = 0; i < vector.size(); i++ )
+	{
+		cout << vector[i] << endl;
+	}
+	*/
+#pragma endregion
+
+#pragma region 리듬 게임
+	srand( time( NULL ) );
+
+	vector<const char*> note;
+	createCount = 5;
+
+	// [←] [↑] [↓] [→]
+	for ( int i = 0; i < createCount; i++ )
+	{
+		int random = rand() % 4;
+
+		switch ( random )
+		{
+		case 0:note.push_back( "↑" );
+			break;
+		case 1:note.push_back( "←" );
+			break;
+		case 2:note.push_back( "→" );
+			break;
+		case 3:note.push_back( "↓" );
+			break;
+		}
+	}
+
+	for ( int i = 0; i < note.size(); i++ )
+	{
+		cout << note[i] << " ";
+	}
+
+
+#pragma endregion
+
 
 	return 0;
 }
