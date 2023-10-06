f9dff8ced1c38bc4bbe6a3ccf9c6c0bf3d3c1ad9
Yeowol01 <139342008+Yeowol01@users.noreply.github.com>
2023-10-06 19:48:10 +0900

    (2023.10.06) 반복자

--- Solution.cpp
+++ Solution.cpp
@@ -1,49 +1,57 @@
-﻿#include <iostream>
-#include <list>
+#include <iostream>
+#include <map>
+#include <set>
 
 using namespace std;
 
 int main()
 {
-#pragma region 문자열
+#pragma region std::map
 	/*
-	string content = "Content";
+	map<string, int> mapData;
 
-	// const char * name
-	cout << "content 변수의 값 : " << content << endl;
-	cout << "content 변수의 값 : " << content.size() << endl;
-	cout << "content 변수의 값 : " << content.length() << endl;
+	mapData.insert( make_pair( "Sword", 5000 ) );
+	mapData.insert( make_pair( "Armor", 2500 ) );
 
-	content = "League";
-	cout << "content 변수의 값 : " << content << endl;
+	// mapData.find("Sword")
+	// [("Sword", 5000)] [("Armor", 2500)]
 
-	cout << content.find( "Le" ) << endl;
+	map<string, int>::iterator mapIter;
+
+	for ( mapIter = mapData.begin(); mapIter != mapData.end(); mapIter++ )
+	{
+		cout << "KEY : " << mapIter->first << endl;
+		cout << "VALUE : " << mapIter->second << endl;
+
+	}
+
+	if ( mapData.find( "Sword" ) != mapData.end() )
+	{
+		cout << "데이터가 존재합니다." << endl;
+	}
+	else
+	{
+		cout << "Not key found" << endl;
+	}
 	*/
 #pragma endregion
 
+#pragma region std::set
 
-#pragma region 연관 컨테이너
-	// KEY와 VALUE가 하나의 구성으로 이루어진 컨테이너 입니다.
+	set<int> setData;
+
+	// 중복된 값이 들어갔을 때, 중복된 값을 제거하고 다시 저장합니다.
+	setData.insert( 10 );
+	setData.insert( 20 );
+	setData.insert( 30 );
+	setData.insert( 40 );
+
+	set<int>::iterator setIter;
+
+	setIter = setData.begin();
 	
-	list<int> dataList;
-
-	dataList.push_back( 10 );
-	dataList.push_front( 50 );
-	dataList.push_front( 25 );
-	dataList.push_back( 33 );
-
-	// 25 50 10 33
-	// dataList.begin() : 첫 번째 주소로 반환
-	// dataList.end() : 마지막에 있는 그 다음 주소를 반환
-
-	// (begin)              (end)
-	//  [25] [50] [10] [33]
-	list<int>::iterator iter;
-
-	for ( iter = dataList.begin(); iter != dataList.end(); iter++ )
-	{
-		cout << *iter << endl;
-	}
+	setData.erase( setIter++ );
+	cout << *setIter << endl;
 
 #pragma endregion
 
